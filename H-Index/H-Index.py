ca1a6ab7110a3a1d9be177f901639f6d56a2835f
jome1204 <104835042+jome1204@users.noreply.github.com>
2023-01-13 19:07:45 +0300

    Create H-Index.py

---
+++ H-Index/H-Index.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def hIndex(self, citations):
+        size = len(citations)
+        citations.sort()
+
+        if len(citations) == 1 and citations[0] != 0:
+            return 1
+        else:
+            while size>0:
+                temp = len(citations)-size
+                if citations[temp]>=size:
+                    return size
+                size-=1
+        return 0
