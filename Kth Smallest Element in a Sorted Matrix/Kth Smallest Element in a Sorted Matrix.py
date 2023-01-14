94c9a26574ad00d25ed215e3577941ab8c4aac0a
jome1204 <104835042+jome1204@users.noreply.github.com>
2023-01-14 18:59:29 +0300

    Create Kth Smallest Element in a Sorted Matrix.py

---
+++ Kth Smallest Element in a Sorted Matrix/Kth Smallest Element in a Sorted Matrix.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def kthSmallest(self, matrix, k):
+        result=[]
+        for i in matrix:
+            for j in i:
+                result.append(j)
+        result.sort()
+        return result[k-1]
+       
