1f36c408ccae10e69ada7671b29e1481ccb60e8d
jome1204 <104835042+jome1204@users.noreply.github.com>
2023-01-16 18:47:11 +0300

    Create Code For 1.py

---
+++ Code For 1/Code For 1.py
@@ -0,0 +1,7 @@
+Input
+The first line contains three integers n, l, r (0 ≤ n < 250, 0 ≤ r - l ≤ 105, r ≥ 1, l ≥ 1) – initial element and the range l to r.
+
+It is guaranteed that r is not greater than the length of the final list.
+
+Output
+Output the total number of 1s in the range l to r in the final sequence\
